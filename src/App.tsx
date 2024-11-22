05e37dc6f4bf071baa92da6d3e38f70e28cb47c2
felipeOliveira-1 <contato@felipesilva.tec>
2024-11-22 12:54:38 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,8 @@ const signInWithGoogle = async () => {
   try {
     const result = await signInWithPopup(auth, provider);
     // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential?.accessToken;
+    // const credential = GoogleAuthProvider.credentialFromResult(result);
+    // const token = credential?.accessToken;
     // The signed-in user info.
     const user = result.user;
     console.log('User Info:', user);
